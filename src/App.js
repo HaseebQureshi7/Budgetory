dbb28d0c97913724a76631a7cb6f0e62512424ce
Haseeb <qureshihaxeeb2@gmail.com>
2022-07-08 11:43:34 +0530

    adding uncategorized expenses card!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Container } from "react-bootstrap";
 import AddBudgetModal from "./components/AddBudgetModal";
 import AddExpenseModal from "./components/AddExpenseModal";
 import BudgetCard from "./components/BudgetCard";
-import { useBudgets } from "./contexts/BudgetsContext";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState()
@@ -32,7 +33,7 @@ function App() {
             return (
               <BudgetCard key={budget.id} name={budget.name} amount={amount} max={budget.max} onAddExpenseClick={()=> openAddExpenseModal(budget.id)} />)
           })}
-
+              <UncategorizedBudgetCard />
         </div>
       </Container>
       <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -14,12 +14,12 @@ export default function BudgetCard({ name, amount, max, grey, onAddExpenseClick 
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="me-2">{name}</div>
                     <div className="d-flex align-items-baseline">{currencyFormatter.format(amount)}/
-                        <span className="text-muted fs-6 ms-1">{currencyFormatter.format(max)}</span></div>
+                        {max && <span className="text-muted fs-6 ms-1">{currencyFormatter.format(max)}</span>}</div>
                 </Card.Title>
-                <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)}
+                {max && <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)}
                     min={0}
                     max={max}
-                    now={amount}></ProgressBar>
+                    now={amount}></ProgressBar>}
                 <Stack direction="horizontal" gap="2" className="mt-4">
                     <Button variant="outline-primary ms-auto" className="ms-auto" onClick={onAddExpenseClick}>Add Expenses</Button>
                     <Button variant="outline-secondary">View Expenses</Button>
